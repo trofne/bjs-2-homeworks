fee88672c1d20060621db0055cd52e1c95506210
Natasha <trofne@mail.ru>
2023-06-26 10:41:27 +0300

    Готовое домашнее задание по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
+
+"use strict";
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter((user) => user.gender === gender);
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+  const sumAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  return +(sumAge / filteredUsers.length).toFixed(1);
 }
+
