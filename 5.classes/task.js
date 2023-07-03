83c0bcc97f27227a58e751735bf07dd5a36d6125
Natasha <trofne@mail.ru>
2023-07-03 09:46:13 +0300

    Доработала задачу по классам

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -5,181 +5,23 @@ class PrintEditionItem {
     this.name = name;
     this.releaseDate = releaseDate;
     this.pagesCount = pagesCount;
-    this.state = 100;
-    this.type = null;
-  }
-
-  fix() {
-    this.state *= 1.5;
-  }
-
-  set state(newState) {
-    if (newState < 0) {
-      this._state = 0;
-    } else if (newState > 100) {
-      this._state = 100;
-    } else {
-      this._state = newState;
-    }
-  }
-
-  get state() {
-    return this._state;
-  }
-}
-
-class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.type = "magazine";
-  }
-}
-
-class Book extends PrintEditionItem {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
-    this.type = "book";
-  }
-}
-
-class NovelBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "novel";
-  }
-}
-
-class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "fantastic";
-  }
-}
-
-class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "detective";
-  }
-}
-
-"use strict";
-
-class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
-    this.state = 100;
-    this.type = null;
-  }
-
-  fix() {
-    this.state *= 1.5;
-  }
-
-  set state(newState) {
-    if (newState < 0) {
-      this._state = 0;
-    } else if (newState > 100) {
-      this._state = 100;
-    } else {
-      this._state = newState;
-    }
-  }
-
-  get state() {
-    return this._state;
-  }
-}
-
-class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.type = "magazine";
-  }
-}
-
-class Book extends PrintEditionItem {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
-    this.type = "book";
-  }
-}
-
-class NovelBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "novel";
-  }
-}
-
-class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "fantastic";
-  }
-}
-
-class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = "detective";
-  }
-}
-
-// Примеры использования
-const sherlock = new PrintEditionItem(
-  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-  2019,
-  1008
-);
-
-console.log(sherlock.releaseDate); //2019
-console.log(sherlock.state); //100
-sherlock.fix();
-console.log(sherlock.state); //100
-
-const picknick = new FantasticBook(
-  "Аркадий и Борис Стругацкие",
-  "Пикник на обочине",
-  1972,
-  168
-);
-
-console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-picknick.state = 10;
-console.log(picknick.state); //10
-picknick.fix();
-console.log(picknick.state); //15.
-
-
-
-"use strict";
-
-class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
     this._state = 100;
     this.type = null;
   }
 
   fix() {
-    this.state *= 1.5;
+    this.state=this._state*1.5;
   }
 
   set state(newState) {
     if (newState < 0) {
       this._state = 0;
+      return;
     } else if (newState > 100) {
       this._state = 100;
-    } else {
-      this._state = newState;
-    }
+      return;
+    } 
+    this._state = newState;
   }
 
   get state() {
@@ -223,73 +65,50 @@ class DetectiveBook extends Book {
   }
 }
 
-class Library {
-  constructor(name) {
+
+"use strict";
+
+class Library{
+  constructor(name){
     this.name = name;
     this.books = [];
   }
-
-  addBook(book) {
-    if (book.state > 30) {
+  
+  addBook(book){
+    if(book.state > 30)
       this.books.push(book);
-    }
   }
 
-  findBookBy(type, value) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i][type] === value) {
-        return this.books[i];
-      }
+  findBookBy(type, value){
+    let result;
+    if(type == "type"){
+      result =  this.books.find((book)=>{return book.type==value});
+    }else if(type == "author"){
+      result =  this.books.find((book)=>{return book.author==value});
+    }else if(type == "name"){
+      result =  this.books.find((book)=>{return book.name==value});
+    }else if(type == "releaseDate"){
+      result =  this.books.find((book)=>{return book.releaseDate==value});
+    }else if(type == "pagesCount"){
+      result =  this.books.find((book)=>{return book.pagesCount==value});
     }
+    if(result != undefined)
+      return result;
     return null;
   }
 
-  giveBookByName(bookName) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].name === bookName) {
-        return this.books.splice(i, 1)[0];
-      }
+  giveBookByName(bookName){
+    let result = this.books.find((book) => {return book.name==bookName});
+    if (result == undefined){
+      return null;
     }
-    return null;
+    const index = this.books.indexOf(result);
+    if(index > -1){
+      this.books.splice(index, 1);
+    }
+    return result;
   }
 }
-
-// Примеры использования
-const library = new Library("Библиотека имени Ленина");
-
-library.addBook(
-  new DetectiveBook(
-    "Артур Конан Дойл",
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
-  )
-);
-library.addBook(
-  new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
-  )
-);
-library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-library.addBook(new Magazine("Мурзилка", 1924, 60));
-
-console.log(library.findBookBy("name", "Властелин колец")); //null
-console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
-
-console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-const book = library.giveBookByName("Машина времени");
-console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
-console.log(book.state); //100
-book.state = 10;
-console.log(book.state); //10
-book.fix();
-console.log(book.state); //15
-library.addBook(book);
-console.log("Количество книг после возврата: " + library.books.length); //Количество книг после возврата: 4
-
 
 "use strict";
 
@@ -328,13 +147,3 @@ class Student {
   }
 }
 
-// Примеры использования
-const student = new Student("Олег Никифоров");
-student.addMark(5, "химия");
-student.addMark(5, "химия");
-student.addMark(5, "физика");
-student.addMark(4, "физика");
-student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
-console.log(student.getAverageBySubject("физика")); // Средний балл по предмету физика 4.5
-console.log(student.getAverageBySubject("биология")); // Вернёт 0, так как по такому предмету нет никаких оценок.
-console.log(student.getAverage()); // Средний балл по всем предметам 4.75
